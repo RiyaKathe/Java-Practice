dad39e77733371d24b992b55c48ca09a98606d63
RiyaKathe <katheriya2005@gmail.com>
2026-04-07 12:00:05 +0530

    Day1

---
+++ abc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class abc {
+     public static void main(String[] args) {
+
+        int  sum , sub;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a and b values");
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        
+        sum=a+b;
+        sub=a-b;
+        System.out.println("\n Sum ="+ sum + "\n sub = "+ sub);
+        
+    
+    }
+
+    
+}
