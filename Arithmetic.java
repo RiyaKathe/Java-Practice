f24494a87b5680f15c473425df914058521f3127
RiyaKathe <katheriya2005@gmail.com>
2026-04-10 12:53:02 +0530

    Day 2

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+public class Arithmetic {
+    
+    public static void main(String[] args) {
+
+        int a=10, b=20 , sum ,sub;
+        sum=a+b;
+        sub=a-b;
+
+        
+        System.out.println("\n Sum ="+ sum + "\n sub = "+ sub);
+        
+    
+    }
+
+
+    
+}
